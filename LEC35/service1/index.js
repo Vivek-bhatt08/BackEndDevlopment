a20c0482305d01f02b32b26984c9f5d870dc35b0
Vivek-bhatt08 <Vivekbhatt83073@gmail.com>
2025-10-30 15:27:26 +0530

    subscriber & publisher

---
+++ LEC35/service1/index.js
@@ -0,0 +1,10 @@
+const {createClient} = require("redis");
+
+let publisher = createClient();
+
+async function notifyMe() {
+    await publisher.connect();
+    await publisher.PUBLISH("notify_me","data");
+    await publisher.PUBLISH("like","your post is liked by someone")
+}
+notifyMe();
